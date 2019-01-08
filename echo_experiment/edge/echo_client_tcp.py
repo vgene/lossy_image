5077fed4f81be1fcda94add1d31d0a555c93f8a1
Ziyang Xu <ziyangx@princeton.edu>
2019-01-08 06:05:07 +0000

    fix tcp port

--- echo_experiment/edge/echo_client_tcp.py
+++ echo_experiment/edge/echo_client_tcp.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('10.0.2.2', 20008)
+server_address = ('10.0.2.2', 20007)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 count = 0
